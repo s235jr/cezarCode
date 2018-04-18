550b42cc7de2919bf24279e7a4ea32f3e9fde6db
Tomek <tomasz.ciecwierz@gmail.com>
2018-04-18 12:13:21 +0200

    initcommit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String codeOryginal = "abrakadabrazrrrrrrr";
+        int slide = 2;
+        String codAfterSlide = "";
+        String codeAfterDecode = "";
+
+        for (int i = 0; i < codeOryginal.length(); i++) {
+
+            int charAfterSlide = codeOryginal.charAt(i) + slide;
+            if (charAfterSlide > 122) {
+                charAfterSlide = charAfterSlide - 26;
+            }
+            codAfterSlide = codAfterSlide + (char)charAfterSlide;
+        }
+        for (int i = 0; i < codAfterSlide.length(); i++) {
+
+            int charAfterSlide = codAfterSlide.charAt(i) - slide;
+            if (charAfterSlide < 97) {
+                charAfterSlide = charAfterSlide + 26;
+            }
+            codeAfterDecode = codeAfterDecode + (char)charAfterSlide;
+        }
+        System.out.println(codeOryginal);
+        System.out.println(codAfterSlide);
+        System.out.println(codeAfterDecode);
+
+
+
+    }
+}
